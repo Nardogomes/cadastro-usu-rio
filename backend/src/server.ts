c59b2b3324dda3b59510c8d9909ff5b9f0e6d14c
Ednardo Gomes <ednardo@gomes.com>
2022-09-23 15:48:54 -0300

    Configurando as rotas do backend.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const prisma = new PrismaClient();
 
 const PORT = 3333;
@@ -12,18 +16,47 @@ app.get("/users", async (request, response) => {
   return response.json({ users });
 });
 
-app.get("/users/:id", (request, response) => {
-  // const userId = request.params.id;
+app.get("/users/:id", async (request, response) => {
+  const userId = request.params.id;
 
-  return response.json({
-    message: "Usuário",
-  });
+  const user = await prisma.user.findMany({
+    where: {
+      CPF: userId,
+    }
+  })
+
+  return response.json({ user });
 });
 
-app.post("/users", (request, response) => {
-  return response.status(201).json({
-    message: "Usuário salvo.",
-  });
+app.post("/users", async (request, response) => {
+  const body = request.body;
+
+  const dataNascimentoFormated = new Date(body.data_nascimento).toISOString();
+
+  const user = await prisma.user.create({
+    data: {
+      CPF: body.CPF,
+      nome: body.nome,
+      RG: body.RG,
+      data_nascimento: dataNascimentoFormated,
+      nome_mae: body.nome_mae,
+      data_cadastro: body.data_cadastro
+    }
+  })
+
+  return response.status(201).json(user);
+});
+
+app.delete("/users/:id", async (request, response) => {
+  const userId = request.params.id;
+
+  const user = await prisma.user.delete({
+    where: {
+      CPF: userId
+    }
+  })
+
+  return response.json({ user });
 });
 
 app.listen(PORT, () => {
